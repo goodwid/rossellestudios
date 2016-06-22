9bf5d2d4187e59cd9a9b06ae52934d949bc3f8a6
David <goodwid@gmail.com>
2016-06-21 22:04:41 -0700

    changed images

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,11 @@ const gulp = require('gulp');
 const imageResize = require('gulp-image-resize');
 const parallel = require("concurrent-transform");
 const os = require("os");
+const imagemin = require('gulp-imagemin');
 
 
 gulp.task('convert', () =>
-    gulp.src('images/*')
-        .pipe(parallel(
-          imageResize({
-      width : 800,
-      // height : 100,
-      upscale : false,
-      imageMagick: true
-    }), os.cpus().length))
-        .pipe(gulp.dest('sized-images/'))
+    gulp.src('sized-images/*')
+        .pipe(parallel(imagemin()), os.cpus().length)
+        .pipe(gulp.dest('sized-images2/'))
 );
